66b1398eff32433239e4e40e48318d671c49f89d
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-07-09 12:02:44 +0545

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react';
 import fs from 'fs';
 
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync('./.cert/key.pem'),
-      cert: fs.readFileSync('./.cert/cert.pem'),
-    },
-  },
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync('./.cert/key.pem'),
+  //     cert: fs.readFileSync('./.cert/cert.pem'),
+  //   },
+  // },
+
   plugins: [react()],
   assetsInclude: ['**/*.PNG', '**/*.png'], // Include both .PNG and .png
 });
